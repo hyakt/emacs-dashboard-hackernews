d6ca1b9ebab8723dcda1fb5f6c87fd8e34ae0932
Hayato KAJIYAMA <kaji1216@gmail.com>
2018-12-06 17:27:38 +0900

    add: copyright and comment

--- dashboard-hackernews.el
+++ dashboard-hackernews.el
@@ -1,7 +1,28 @@
 ;;; dashboard-hackernews.el --- Hacker News Client for dashboard; -*- lexical-binding: t -*-;
 
+;; Copyright (C) 2018 by Hayato KAJIYAMA
+
+;; Author:  Hayato KAJIYAMA <kaji1216@gmail.com>
+;; URL: https://github.com/hyakt/emacs-dashboard-hackernews
+;; Version: 0.0.1
+;; Package-Requires: ((dashboard "1.2.5") (request "0.3.0"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
-;; Check Hacker News on Emacs.
+
+;; Display Hacker News on Emacs Dashboard.
 
 ;;; Code:
 
@@ -73,5 +94,4 @@
                                           (dashboard-subseq dashboard-hackernews-items 0 list-size))))
 
 (provide 'dashboard-hackernews)
-
 ;;; dashboard-hackernews.el ends here
